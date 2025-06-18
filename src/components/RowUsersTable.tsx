f425bc83531ee955f45fcbaaf3f0192ecc333052
Timofei Mamaev <timofeiisrael@gmail.com>
2025-06-18 17:18:41 +0300

    I continue to do model User

--- src/components/RowUsersTable.tsx
+++ src/components/RowUsersTable.tsx
@@ -31,9 +31,9 @@ const RowUsersTable = (props: PropsUser) => {
             <th className={"font-normal w-70"}>{user.firstName}</th>
             <th className={"font-normal w-70"}>{user.lastName}</th>
             <th className={"font-normal w-70"}>{user.email}</th>
-            <th className={"font-normal w-70"}>{user.address}</th>
+            <th className={"font-normal w-70"}>{user.address.city},{user.address.street}, {user.address.houseNumber} </th>
             <th className={"font-normal w-70"}>{user.role}</th>
-            <th className={"font-normal w-70"}>{user.orders}</th>
+            <th className={"font-normal w-70"}>{user.orders.length} orders</th>
             <th className={"w-5 flex flex-row flex-nowrap justify-around items-center"}>
                 <img src={"./src/assets/edit.jpg"} alt={"Edit"} className={"w-5"}
                      onClick={() => editUser(user.login)}></img>
