a40590967f3631f7fffadd8d5778e1e1341a8202
Timofei Mamaev <timofeiisrael@gmail.com>
2025-10-20 03:20:02 +0300

    starting doing responsive design

--- src/components/pages/home/MainLayout.tsx
+++ src/components/pages/home/MainLayout.tsx
@@ -4,22 +4,22 @@ import Footer from "./Footer";
 
 const MainLayout = () => {
     return (
-        <div className="flex flex-col min-h-screen font-['Rubik']">
+        <div className="flex flex-col min-h-screen font-['Rubik'] max-w-[1440px]">
 
-            <div className="sticky top-0 z-50 bg-white w-full">
-                <div className="max-w-[1640px] mx-auto my-6">
+            <div className="flex-1 sticky top-0 z-50 bg-white w-full">
+                <div className="mx-2 my-6">
                     <MegaMenu/>
                 </div>
             </div>
 
             <main className="flex-1">
-                <div className="max-w-[1640px] mx-auto">
+                <div className="w-full mx-2">
                     <Outlet/>
                 </div>
             </main>
 
-            <div className="top-0 z-50">
-                <div className="max-w-[1640px] mx-auto">
+            <div className="flex-1 top-0 z-50">
+                <div className="w-full mx-2">
                     <Footer/>
                 </div>
             </div>
